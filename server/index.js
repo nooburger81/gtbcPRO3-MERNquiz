b38b1ba4bdcd4adaf9001bb6fc597ce1b48f6240
nooburger81 <brandonsebastian@gmail.com>
2023-06-13 15:42:34 -0400

    cleaned up client folder

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 let server;
+
+//middleware config
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
+app.use(bodyParser.json({ limit: '20mb' }));
 
 mongoose.connect(process.env.DB_URI, {
     useUnifiedTopology: true,
