0dbb6a41a0d3a04589f0ac4b5f22778adb65bc7c
nooburger81 <brandonsebastian@gmail.com>
2023-06-14 12:38:24 -0400

    css fun test

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import 'animate.css'
 
 const Home =  () => {
   return (
-    <div className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
-        PRESTIGE WORLDWIDE ©️ 2023
+    <div name='home' className='w-full h-screen bg-pink-400'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+    <h1 className='text-4xl sm:text-7xl font-bold text-[#90d36e] flex justify-center animate__animated animate__fadeInDown animate__delay-1s'>
+        Whatchoo Know 'Bout Me?
+    </h1>
+    <h2 className='text-4xl sm:text-7xl font-bold text-[#4195ae] flex justify-center animate__animated animate__bounceInUp animate__delay-2s '>
+          A Friendly Quiz Game
+        </h2>
+    </div>
     </div>
   );
 }
