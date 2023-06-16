af4b4d5f265a37ed0811b3cfa293e219259fa9cc
nooburger81 <brandonsebastian@gmail.com>
2023-06-16 12:04:21 -0400

    setting up apolloserver and graphql

--- server/index.js
+++ server/index.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/users');
 const models = require('./models/Users');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('/Users/maeven/Desktop/gtbcPRO3-MERNquiz/server/schema');
 require('dotenv').config();
 
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers 
+});
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const PORT = process.env.PORT || 3001;
+const startApolloServer = async () => {
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+};
 let server;
 
 //middleware config
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
-app.use(bodyParser.json({ limit: '20mb' }));
+// app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
+// app.use(bodyParser.json({ limit: '20mb' }));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
@@ -35,3 +51,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernquiz-
  server = app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
+
+startApolloServer();
